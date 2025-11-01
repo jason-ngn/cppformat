40222028e56477c719ee5b002056e44f544f333b
Jason <bennguyen20122006@gmail.com>
2025-10-31 21:07:31 -0700

    Modified cppformat

--- src/main.js
+++ src/main.js
@@ -1,27 +1,15 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import { getExecOutput } from '@actions/exec'
 
-/**
- * The main function for the action.
- *
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export async function run() {
+export const run = async () => {
   try {
-    const ms = core.getInput('milliseconds')
+    const { stdout } = await getExecOutput(
+      'clang-format -style=Google -i *.cc *.h'
+    )
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    core.info(stdout)
+  } catch (err) {
+    core.error(err)
+    core.setFailed('Your code is not formatted properly.')
   }
 }
